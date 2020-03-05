b094fd68428642794d521c0bf285e8c3840a429b
pleugemors <pleugemors@yahoo.com>
2020-03-05 02:17:41 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    array_of_roles = []
+    self.characters.each do |character|
+        array_of_roles << character.name + " - " + character.show.name
+    end
+    array_of_roles
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  
+  def actors_list
+    actors_names = []
+    self.characters.each{|character| actors_names << character.actor.full_name}
+    actors_names
+  end
+
   
 end
